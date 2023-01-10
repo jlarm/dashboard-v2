16c90844028dab3b19dddd7cdc7e85671883eb4b
Joe Lohr <jlohr@autorisknow.com>
2023-01-10 14:11:13 -0600

    Enable Universal Routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,8 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
+        'universal' => [],
+
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
