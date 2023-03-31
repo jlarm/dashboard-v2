486dcb8c3430fa5d1b57969c5565e26e3065a6cc
Joe Lohr <jlohr@autorisknow.com>
2023-03-31 15:01:03 -0500

    Temp adjustment to dealership roles table

--- database/migrations/2023_03_20_192016_create_dealership_roles_table.php
+++ database/migrations/2023_03_20_192016_create_dealership_roles_table.php
@@ -16,13 +16,13 @@ return new class extends Migration
         Schema::create('dealership_roles', function (Blueprint $table) {
             $table->id();
 
-            $table->string('tenant_id');
-            $table->string('global_role_id');
-
-            $table->unique(['tenant_id', 'global_role_id']);
-
-            $table->foreign('tenant_id')->references('id')->on('tenants')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('global_role_id')->references('global_id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
+//            $table->string('tenant_id');
+//            $table->string('global_role_id');
+//
+//            $table->unique(['tenant_id', 'global_role_id']);
+//
+//            $table->foreign('tenant_id')->references('id')->on('tenants')->onUpdate('cascade')->onDelete('cascade');
+//            $table->foreign('global_role_id')->references('global_id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
 
         });
     }
