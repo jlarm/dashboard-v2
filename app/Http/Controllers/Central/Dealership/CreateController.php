f5cf91a06d82e87b403cd8200d7621745c493c0d
Joe Lohr <jlohr@autorisknow.com>
2023-04-04 08:52:05 -0500

    Display error if dealership creation fails

--- app/Http/Controllers/Central/Dealership/CreateController.php
+++ app/Http/Controllers/Central/Dealership/CreateController.php
@@ -16,33 +16,37 @@ class CreateController extends Controller
 
         $tenantDomain = $validated['domain'].'.'.config('tenancy.central_domains')[0];
 
-        $dealer = Dealership::create([
-            'user_id' => auth()->user()->id,
-            'name' => $validated['name'],
-            'address' => $validated['address'],
-            'city' => $validated['city'],
-            'state' => $validated['state'],
-            'zip_code' => $validated['zip_code'],
-            'phone' => $validated['phone'],
-            'fax' => $validated['fax'],
-            'domain' => $tenantDomain,
-            'url' => $validated['url'],
-            'locations' => $validated['locations'],
-        ]);
-
-        $dealer->createDomain($tenantDomain, $validated['url']);
-
-        $pass = $validated['password'];
-
-        $dealer->run(function () use ($pass) {
-            $user = User::create([
-                'name' => auth()->user()->name,
-                'email' => auth()->user()->email,
-                'phone' => auth()->user()->phone,
-                'password' => bcrypt($pass),
+        try {
+            $dealer = Dealership::create([
+                'user_id' => auth()->user()->id,
+                'name' => $validated['name'],
+                'address' => $validated['address'],
+                'city' => $validated['city'],
+                'state' => $validated['state'],
+                'zip_code' => $validated['zip_code'],
+                'phone' => $validated['phone'],
+                'fax' => $validated['fax'],
+                'domain' => $tenantDomain,
+                'url' => $validated['url'],
+                'locations' => $validated['locations'],
             ]);
-            $user->assignRole('Consultant');
-        });
+
+            $dealer->createDomain($tenantDomain, $validated['url']);
+
+            $pass = $validated['password'];
+
+            $dealer->run(function () use ($pass) {
+                $user = User::create([
+                    'name' => auth()->user()->name,
+                    'email' => auth()->user()->email,
+                    'phone' => auth()->user()->phone,
+                    'password' => bcrypt($pass),
+                ]);
+                $user->assignRole('Consultant');
+            });
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
 
         return redirect(route('dealerships.index'));
     }
--- resources/views/central/dealership/index.blade.php
+++ resources/views/central/dealership/index.blade.php
@@ -14,6 +14,13 @@
 
     <div class="py-12 px-5 lg:px-0">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if(session('error'))
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-10"
+                     role="alert">
+                    <strong class="font-bold">Error!</strong>
+                    <span class="block sm:inline">{{ session('error') }}</span>
+                </div>
+            @endif
             @can('delete-users')
                 <livewire:central.dealership.index/>
             @endcan
