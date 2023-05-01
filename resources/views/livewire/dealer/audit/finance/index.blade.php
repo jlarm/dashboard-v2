5a5bbf462e2ed7645b6174f7bcd11f231166ee44
Joe Lohr <jlohr@autorisknow.com>
2023-05-01 12:16:24 -0500

    updated finance audit create route

--- resources/views/livewire/dealer/audit/finance/index.blade.php
+++ resources/views/livewire/dealer/audit/finance/index.blade.php
@@ -7,7 +7,7 @@
             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                 <a
                     class="inline-flex items-center px-4 py-2 bg-arm-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-arm-blue-700 focus:bg-arm-blue-700 active:bg-arm-blue-900 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                    href="{{ route('dealer.audit.osha.create') }}"
+                    href="{{ route('dealer.audit.finance.create') }}"
                 >
                     Create Audit
                 </a>
