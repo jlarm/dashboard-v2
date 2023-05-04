69026f4b20e66a5b067b249b9f95750841ddb61e
Joe Lohr <jlohr@autorisknow.com>
2023-05-04 11:49:23 -0500

    Date field adjustment on audit form

--- resources/views/dealer/audit/finance/show.blade.php
+++ resources/views/dealer/audit/finance/show.blade.php
@@ -8,7 +8,7 @@
     </div>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto px-3">
+        <div class="max-w-7xl mx-auto px-0 sm:px-3">
             <livewire:dealer.audit.finance.show :financeAudit="$financeAudit"/>
         </div>
     </div>
--- resources/views/livewire/dealer/audit/finance/show.blade.php
+++ resources/views/livewire/dealer/audit/finance/show.blade.php
@@ -1,6 +1,6 @@
 <form>
     <div class="space-y-5">
-        <div>
+        <div class="ml-3 sm:ml-0">
             <label for="audit_date" class="block text-sm font-medium leading-6 text-gray-900">Audit Date</label>
             <div class="mt-2">
                 <input
@@ -9,7 +9,7 @@
                     name="audit_date"
                     id="audit_date"
                     pattern=""
-                    class="block w-1/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-arm-blue-600 sm:text-sm sm:leading-6"
+                    class="block w-1/2 sm:w-1/3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-arm-blue-600 sm:text-sm sm:leading-6"
                 />
             </div>
         </div>
