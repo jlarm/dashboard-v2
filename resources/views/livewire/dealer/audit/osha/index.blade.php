b357e492e2de42e97ed385b2e7b4945d0d98554b
Joe Lohr <jlohr@autorisknow.com>
2023-05-15 08:19:08 -0500

    Updated index views on osha and body shop

--- resources/views/livewire/dealer/audit/osha/index.blade.php
+++ resources/views/livewire/dealer/audit/osha/index.blade.php
@@ -1,50 +1,32 @@
-<div>
-    <div class="px-4 sm:px-6 lg:px-8">
-        <div class="sm:flex sm:items-center">
-            <div class="sm:flex-auto">
-                <h1 class="text-lg font-medium leading-6 text-gray-900">OSHA Audits</h1>
-            </div>
-            <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                <a
-                    class="inline-flex items-center px-4 py-2 bg-arm-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-arm-blue-700 focus:bg-arm-blue-700 active:bg-arm-blue-900 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                    href="{{ route('dealer.audit.osha.create') }}"
-                >
-                    Create Audit
-                </a>
-            </div>
-        </div>
-        <div class="mt-8 flow-root">
-            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="inline-block min-w-full py-2 align-middle">
-                    <table class="min-w-full divide-y divide-gray-300">
-                        <thead>
-                        <tr>
-                            <th scope="col"
-                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">
-                                Date
-                            </th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status
-                            </th>
-                            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">
-                                <span class="sr-only">Edit</span>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody class="divide-y divide-gray-200 bg-white">
-                        @forelse($oshaAudits as $oshaAudit)
-                            <livewire:dealer.audit.osha.index-item :oshaAudit="$oshaAudit"/>
-                        @empty
-                            <tr>
-                                <td colspan="7"
-                                    class="px-4 py-4 text-center text-xl text-arm-blue-500 font-medium sm:pr-6 space-x-3">
-                                    No Audits Created
-                                </td>
-                            </tr>
-                        @endforelse
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+<div class="w-full bg-white">
+    <div>
+        <div class="inline-block min-w-full align-middle">
+            <table class="min-w-full divide-y divide-gray-300">
+                <thead>
+                <tr>
+                    <th scope="col"
+                        class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Date
+                    </th>
+                    @can('create-audits')
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"><span
+                                class="sr-only">Edit</span></th>
+                    @endcan
+                </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                @forelse($oshaAudits as $oshaAudit)
+                    <livewire:dealer.audit.osha.index-item :oshaAudit="$oshaAudit"/>
+                @empty
+                    <tr>
+                        <td colspan="7"
+                            class="px-4 py-4 text-center text-xl text-arm-blue-500 font-medium sm:pr-6 space-x-3">
+                            No Audits Created
+                        </td>
+                    </tr>
+                @endforelse
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
