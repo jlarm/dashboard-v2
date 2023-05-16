ba2ea04b3c61d16fe057e9a563a38f3a93e36ecd
Joe Lohr <jlohr@autorisknow.com>
2023-05-16 11:46:47 -0500

    Setup create on bodyshop create class

--- app/Http/Livewire/Dealer/Store/SingleStore/Audit/BodyShop/Create.php
+++ app/Http/Livewire/Dealer/Store/SingleStore/Audit/BodyShop/Create.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Livewire\Dealer\Store\SingleStore\Audit\BodyShop;
 
+use App\Models\Dealer\Audit\BodyShopAudit;
 use App\Models\Dealer\Store;
 use Livewire\Component;
 
 class Create extends Component
 {
     public Store $store;
-    public function render()
+    public function mount()
     {
-        return view('livewire.dealer.store.single-store.audit.body-shop.create')->layout('components.dealer-app');
+        $audit = BodyShopAudit::create([
+            'user_id' => auth()->id(),
+            'store_id' => $this->store->id ?? Store::first()->id,
+            'audit_date' => now()->format('Y-m-d'),
+        ]);
+
+        return redirect()->to(route('dealer.stores.audits.body-shop.show', [$this->store, $audit->id]));
     }
 }
