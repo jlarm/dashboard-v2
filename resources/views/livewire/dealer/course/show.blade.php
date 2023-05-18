6b9d18977d8b2172b70a49f5345efe8b44fc353d
Joe Lohr <jlohr@autorisknow.com>
2023-05-18 10:17:57 -0500

    Updated quiz slides to have pagination on top and quiz button on last slide

--- resources/views/livewire/dealer/course/show.blade.php
+++ resources/views/livewire/dealer/course/show.blade.php
@@ -1,92 +1,66 @@
 <div>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide@3.5.x/dist/css/glide.core.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide@3.5.x"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/css/splide.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/js/splide.min.js"></script>
 
     <style>
-        .glide__bullet--active {
+        .splide__pagination__page.is-active {
             background: #333;
+        }
+
+        .splide__pagination {
+            top: 0;
+            bottom: auto;
+        }
+
+        .splide__track {
+            padding-top: 3em;
+        }
+
+        .splide__arrows {
+            height: 2em;
+            margin: 0 auto;
+            position: relative;
+            top: -0.4em;
         }
     </style>
 
     <div
         x-data="{
         init() {
-            new Glide(this.$refs.glide, {
-                perView: 1,
-                640: {
-                    perView: 1,
-                },
+            new Splide(this.$refs.splide, {
+                gap: '1rem',
+                autoHeight: true,
+                keyboard: true,
+                wheel: true,
+                focus: 'center',
             }).mount()
         },
     }"
     >
-        <div x-ref="glide" class="glide block relative px-12">
-            <div class="glide__track" data-glide-el="track">
-                <ul class="glide__slides">
+        <section x-ref="splide" class="splide">
+            <div class="splide__track">
+                <ul class="splide__list items-baseline">
                     @foreach($course['slides'] as $slide)
-                        <li class="glide__slide flex flex-col items-center justify-center p-6">
+                        <li class="splide__slide flex flex-col items-center justify-center pb-8">
                             <h2 class="text-2xl font-bold mb-5">{{ $slide['title'] ?? '' }}</h2>
                             <span class="prose">
                                 {!! $slide['description'] !!}
                             </span>
                         </li>
                     @endforeach
+                    <li class="splide__slide flex flex-col items-center justify-center pb-8">
+                        <div class="w-full px-5 flex justify-center">
+                            <a
+                                href="{{ route('dealer.courses.quiz', $course) }}"
+                                class="inline-flex items-center rounded-md border border-transparent bg-arm-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-arm-green-700 focus:outline-none focus:ring-2 focus:ring-arm-green-500 focus:ring-offset-2"
+                            >
+                                Start Quiz
+                            </a>
+                        </div>
+                    </li>
                 </ul>
             </div>
-
-            <div class="glide__arrows pointer-events-none absolute inset-0 flex items-center justify-between"
-                 data-glide-el="controls">
-                <!-- Previous Button -->
-                <button
-                    class="glide__arrow glide__arrow--left pointer-events-auto disabled:opacity-50"
-                    data-glide-dir="<"
-                >
-                <span aria-hidden="true">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-gray-600" fill="none"
-                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="3"><path stroke-linecap="round"
-                                                                                          stroke-linejoin="round"
-                                                                                          d="M15 19l-7-7 7-7"/></svg>
-                </span>
-                    <span class="sr-only">Skip to previous slide page</span>
-                </button>
-
-                <!-- Next Button -->
-                <button
-                    class="glide__arrow glide__arrow--left pointer-events-auto disabled:opacity-50"
-                    data-glide-dir=">"
-                >
-                <span aria-hidden="true">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-gray-600" fill="none"
-                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="3"><path stroke-linecap="round"
-                                                                                          stroke-linejoin="round"
-                                                                                          d="M9 5l7 7-7 7"/></svg>
-                </span>
-                    <span class="sr-only">Skip to next slide page</span>
-                </button>
-            </div>
-
-            <!-- Bullets -->
-            <div class="glide__bullets flex w-full items-center justify-center gap-1" data-glide-el="controls[nav]">
-                @foreach($course['slides'] as $bullet)
-                    @php
-                        $i = 0
-                    @endphp
-                    <button class="glide__bullet h-3 w-3 rounded-full bg-gray-300 transition-colors"
-                            data-glide-dir="={{ $i }}"></button>
-                    @php
-                        $i++
-                    @endphp
-                @endforeach
-            </div>
-        </div>
-        <div class="w-full px-5 mt-10 flex justify-center">
-            <a
-                href="{{ route('dealer.courses.quiz', $course) }}"
-                class="inline-flex items-center rounded-md border border-transparent bg-arm-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-arm-green-700 focus:outline-none focus:ring-2 focus:ring-arm-green-500 focus:ring-offset-2"
-            >
-                Start Quiz
-            </a>
-        </div>
+        </section>
     </div>
 
 </div>
