eb23bcbe80f3d6012d08da70a54d7d4d0d80bdab
Joe Lohr <jlohr@autorisknow.com>
2023-05-25 09:15:27 -0500

    added ios icon update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="{{ asset('storage/favicon.svg') }}" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('storage/ios-icon.png') }}">
+    <link rel="shortcut icon" href="{{ url('/favicon.svg') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('/ios-icon.png') }}">
 
     <title>@if(tenant('company'))
             {{ tenant('company') }} |
