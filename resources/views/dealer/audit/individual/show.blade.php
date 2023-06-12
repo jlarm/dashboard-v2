1a713fbf61c205770b9383bdf408370b94b6a267
Joe Lohr <jlohr@autorisknow.com>
2023-06-12 18:03:38 -0500

    Hide deal jacket download until pdf generated

--- resources/views/dealer/audit/individual/show.blade.php
+++ resources/views/dealer/audit/individual/show.blade.php
@@ -6,9 +6,9 @@
                 {{ $individualAudit->audit_date->format('F d, Y') }}</h1>
         </div>
         <div class="mt-4 flex sm:mt-0 sm:ml-4 space-x-5">
-            {{--            @if($individualAudit->pdf_path)--}}
-            <livewire:dealer.audit.individual.download :individualAudit="$individualAudit"/>
-            {{--            @else--}}
+            @if($individualAudit->pdf_path)
+                <livewire:dealer.audit.individual.download :individualAudit="$individualAudit"/>
+            @endif
             <a class="inline-flex items-center px-4 py-2 bg-arm-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-arm-blue-700 focus:bg-arm-blue-700 active:bg-arm-blue-900 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2 transition ease-in-out duration-150"
                href="{{ route('dealer.audit.individual.create', $individualAudit) }}">
                 Create Audit
@@ -17,7 +17,6 @@
             @if(!$drafts)
                 <livewire:dealer.audit.individual.generate :individualAudit="$individualAudit"/>
             @endif
-            {{--            @endif--}}
         </div>
     </div>
 
