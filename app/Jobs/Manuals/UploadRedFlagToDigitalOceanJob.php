570ad294b16dd52e851f186575afd3fe15b71efa
Joe Lohr <jlohr@autorisknow.com>
2023-06-14 14:28:44 -0500

    Added Red Flags Manual upload job

---
+++ app/Jobs/Manuals/UploadRedFlagToDigitalOceanJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs\Manuals;
+
+use App\Models\Dealer\Manual\RedFlag;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+
+class UploadRedFlagToDigitalOceanJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(protected RedFlag $manual)
+    {
+    }
+
+    public function middleware(): array
+    {
+        return [new WithoutOverlapping($this->manual)];
+    }
+
+    public function handle()
+    {
+        $pdf = Storage::get('/' . $this->manual->pdf_path);
+        $moved = Storage::disk('do-manuals')->put(tenant('id') . '/red-flags/' . $this->manual->pdf_path, $pdf);
+        if($moved) {
+            Storage::delete('/' . $this->manual->pdf_path);
+        }
+    }
+}
