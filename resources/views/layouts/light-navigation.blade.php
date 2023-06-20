ad7c165a5cf2c4a5a36ca9f2edad0de511134f37
Joe Lohr <jlohr@autorisknow.com>
2023-06-20 11:11:42 -0500

    Hide manuals in main nav if multi store

--- resources/views/layouts/light-navigation.blade.php
+++ resources/views/layouts/light-navigation.blade.php
@@ -361,21 +361,23 @@
                         </a>
                     @endcan
                     <!-- MANUALS -->
-                    @can('create-stores')
-                        <a
-                            href="{{ route('dealer.manual.index') }}"
-                            class="{{ (request()->segment(1) == 'manuals') ? 'bg-arm-blue-50 text-arm-blue-600 border-arm-blue-600' : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50' }} border-transparent group border-l-4 py-2 px-3 flex items-center text-sm font-medium"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                 stroke-width="1.5"
-                                 class="{{ (request()->segment(1) == 'manuals') ? 'text-arm-blue-500' : 'text-gray-400 group-hover:text-gray-500' }} mr-3 flex-shrink-0 h-6 w-6"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"/>
-                            </svg>
-                            Manuals
-                        </a>
-                    @endcan
+                    @if(!tenant('locations'))
+                        @can('create-stores')
+                            <a
+                                href="{{ route('dealer.manual.index') }}"
+                                class="{{ (request()->segment(1) == 'manuals') ? 'bg-arm-blue-50 text-arm-blue-600 border-arm-blue-600' : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50' }} border-transparent group border-l-4 py-2 px-3 flex items-center text-sm font-medium"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5"
+                                     class="{{ (request()->segment(1) == 'manuals') ? 'text-arm-blue-500' : 'text-gray-400 group-hover:text-gray-500' }} mr-3 flex-shrink-0 h-6 w-6"
+                                     stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"/>
+                                </svg>
+                                Manuals
+                            </a>
+                        @endcan
+                    @endif
                     <!-- SCANS -->
                     @if(!tenant('locations'))
                         @can('create-stores')
