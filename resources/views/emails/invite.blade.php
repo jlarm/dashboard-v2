bd29800014d76bb1c854da55fc9085b654a7b825
Joe Lohr <jlohr@autorisknow.com>
2023-06-21 14:32:04 -0500

    Fixed indent in mail template

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,13 +1,11 @@
+<!-- prettier-ignore -->
 @component('mail::message')
-    # Hi {{ $name }}
-
-    You have been invited to join the {{ $company }} compliance dashboard. Please click the link below to finish your registration.
-
-    @component('mail::button', ['url' => $link])
-        Complete Registration
-    @endcomponent
-
-    Thanks,
-    {{ $company }}
+# Hi {{ $name }}
+You have been invited to join the {{ $company }} compliance dashboard. Please click the link below to finish your registration.
+@component('mail::button', ['url' => $link])
+    Complete Registration
+@endcomponent
+Thanks,
+{{ $company }}
 @endcomponent
 
