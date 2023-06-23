a75dfdba789d8d0ba54a15fd5954e71aad073548
Joe Lohr <jlohr@autorisknow.com>
2023-06-23 10:39:57 -0500

    Removed status in body shop and glba

--- resources/views/livewire/dealer/store/single-store/audit/finance/index.blade.php
+++ resources/views/livewire/dealer/store/single-store/audit/finance/index.blade.php
@@ -24,8 +24,6 @@
                                 class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">
                                 Date
                             </th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status
-                            </th>
                             <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">
                                 <span class="sr-only">Edit</span>
                             </th>
