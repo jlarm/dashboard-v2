c65b2b09ed66de647f58f01d3b607fea5586ba30
Joe Lohr <jlohr@autorisknow.com>
2023-07-05 13:07:40 -0500

    Hide search for courses index

--- resources/views/livewire/dealer/course/index.blade.php
+++ resources/views/livewire/dealer/course/index.blade.php
@@ -1,14 +1,14 @@
 <div>
     <div class="space-y-4">
-        <div class="md:w-1/3 px-3 sm:px-0">
-            <div>
-                <label for="search" class="sr-only">Search</label>
-                <input type="search" name="search" id="search"
-                       wire:model="search"
-                       class="block w-full rounded-md border-gray-300 shadow-sm focus:border-arm-blue-500 focus:ring-arm-blue-500 sm:text-sm"
-                       placeholder="Search Courses...">
-            </div>
-        </div>
+        {{--        <div class="md:w-1/3 px-3 sm:px-0">--}}
+        {{--            <div>--}}
+        {{--                <label for="search" class="sr-only">Search</label>--}}
+        {{--                <input type="search" name="search" id="search"--}}
+        {{--                       wire:model="search"--}}
+        {{--                       class="block w-full rounded-md border-gray-300 shadow-sm focus:border-arm-blue-500 focus:ring-arm-blue-500 sm:text-sm"--}}
+        {{--                       placeholder="Search Courses...">--}}
+        {{--            </div>--}}
+        {{--        </div>--}}
         <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 px-3 sm:px-0">
             @forelse($courses as $course)
                 <livewire:dealer.course.index-item :course="$course" :key="$course->id"/>
