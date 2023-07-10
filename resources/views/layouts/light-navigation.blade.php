d581b0d8c00f6127c851a8c53ed68d92b211e64e
Joe Lohr <jlohr@autorisknow.com>
2023-07-10 14:21:26 -0500

    Update roles to access scans

--- resources/views/layouts/light-navigation.blade.php
+++ resources/views/layouts/light-navigation.blade.php
@@ -380,7 +380,7 @@
                     @endif
                     <!-- SCANS -->
                     @if(!tenant('locations'))
-                        @can('create-stores')
+                        @can('view-scans')
                             <a
                                 href="{{ route('dealer.scan.index') }}"
                                 class="{{ (request()->segment(1) == 'scans') ? 'bg-arm-blue-50 text-arm-blue-600 border-arm-blue-600' : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50' }} border-transparent group border-l-4 py-2 px-3 flex items-center text-sm font-medium"
