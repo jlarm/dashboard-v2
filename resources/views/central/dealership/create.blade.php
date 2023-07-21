148ad05fc2d635aa033a6c4f845982f219d7aaed
Joe Lohr <jlohr@autorisknow.com>
2023-07-21 09:48:29 -0500

    Update to creating dealership form

--- resources/views/central/dealership/create.blade.php
+++ resources/views/central/dealership/create.blade.php
@@ -95,16 +95,16 @@
                             <div>
                                 <x-input-label for="domain" :value="__('Domain')"/>
                                 <div class="flex items-center">
-                                    <x-text-input
+                                    <input
+                                        class="border-gray-300 focus:border-gray-300 focus:ring-gray-50 rounded-l-md block mt-1 w-full"
+                                        type="text"
                                         wire:model.defer="domain"
                                         id="domain"
-                                        class="block mt-1 w-full"
-                                        type="text"
                                         name="domain"
-                                        :value="old('domain')"
                                         required
-                                    />
-                                    <span>.{{ config('tenancy.central_domains')[0] }}</span>
+                                    >
+                                    <span
+                                        class="flex items-center rounded-r-md border border-gray-300 border-l-0 bg-slate-50 focus:border-arm-blue-500 focus:ring-arm-blue-500 px-2 py-2 mt-1 text-slate-400 transition-colors duration-300 peer-focus:border-sky-400 peer-focus:bg-sky-400 peer-focus:text-white">.{{ config('tenancy.central_domains')[0] }}</span>
                                 </div>
                                 <x-input-error :messages="$errors->get('domain')" class="mt-2"/>
                             </div>
