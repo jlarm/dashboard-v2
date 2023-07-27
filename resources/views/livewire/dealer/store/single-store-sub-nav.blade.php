1612929bca83a996defe60f0f2db16dd6e3db069
Joe Lohr <jlohr@autorisknow.com>
2023-07-27 15:38:11 -0500

    Added x-cloak to sub nav dropdown

--- resources/views/livewire/dealer/store/single-store-sub-nav.blade.php
+++ resources/views/livewire/dealer/store/single-store-sub-nav.blade.php
@@ -36,15 +36,19 @@
                                   clip-rule="evenodd"></path>
                         </svg>
                     </button>
-                    <div x-show="open" x-transition:enter="transition ease-out duration-200"
-                         x-transition:enter-start="opacity-0 translate-y-1"
-                         x-transition:enter-end="opacity-100 translate-y-0"
-                         x-transition:leave="transition ease-in duration-150"
-                         x-transition:leave-start="opacity-100 translate-y-0"
-                         x-transition:leave-end="opacity-0 translate-y-1"
-                         x-description="'Company' flyout menu, show/hide based on flyout menu state."
-                         class="x-cloak absolute -left-8 top-full z-10 mt-3 w-56 rounded-xl bg-white p-2 shadow-lg ring-1 ring-gray-900/5"
-                         x-ref="panel" @click.away="open = false">
+                    <div
+                        x-transition:enter="transition ease-out duration-200"
+                        x-transition:enter-start="opacity-0 translate-y-1"
+                        x-transition:enter-end="opacity-100 translate-y-0"
+                        x-transition:leave="transition ease-in duration-150"
+                        x-transition:leave-start="opacity-100 translate-y-0"
+                        x-transition:leave-end="opacity-0 translate-y-1"
+                        x-show="open"
+                        class="x-cloak absolute -left-8 top-full z-10 mt-3 w-56 rounded-xl bg-white p-2 shadow-lg ring-1 ring-gray-900/5"
+                        x-ref="panel"
+                        @click.away="open = false"
+                        x-cloak
+                    >
                         <a href="{{ route('dealer.stores.audits.osha.index', $store) }}"
                            class="block rounded-lg px-3 py-2 text-sm leading-6 text-arm-blue-500 hover:bg-arm-blue-50">OSHA</a>
                         <a href="{{ route('dealer.stores.audits.body-shop.index', $store) }}"
