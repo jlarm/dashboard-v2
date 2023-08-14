3acf84ec712e7e501e73deab8fb2d15e88033907
Joe Lohr <jlohr@autorisknow.com>
2023-08-14 14:54:57 -0500

    Show completed courses for consultants

--- app/Http/Livewire/Central/Employee/IndexItem.php
+++ app/Http/Livewire/Central/Employee/IndexItem.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Livewire\Central\Employee;
 
+use App\Models\Course;
 use App\Models\User;
+use DB;
 use Livewire\Component;
 
 class IndexItem extends Component
 {
     public User $user;
+    public int $totalCourses;
+    public $completed;
+
+    public function mount()
+    {
+        $this->totalCourses = Course::count();
+
+        $this->completed = DB::table('course_results')
+            ->where('user_id', $this->user->id)
+            ->where('created_at', '>=', now()->subYear())
+            ->latest()
+            ->get()
+            ->groupBy('course_id')
+            ->map(function ($item) {
+                return $item->first();
+            });
+
+        $this->completed = collect($this->completed->where('passed', 1))->count();
+    }
 
     public function render()
     {
--- resources/views/livewire/central/employee/index-item.blade.php
+++ resources/views/livewire/central/employee/index-item.blade.php
@@ -2,6 +2,7 @@
     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ $user->name }}</td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $user->email }}</td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $user->phoneNumber }}</td>
+    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $completed }} of {{ $totalCourses }} passed</td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
         @if( $user->roles->first()->name == 'super-admin')
             <span
