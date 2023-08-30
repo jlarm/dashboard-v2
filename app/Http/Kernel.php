f24472a5d4f864719a28878062d8ba4527bc3547
Joe Lohr <jlohr@autorisknow.com>
2023-08-30 09:04:04 -0500

    update to kernel for middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'has.stores' => \App\Http\Middleware\CheckStoreStatusMiddleware::class,
+        'single.store' => \App\Http\Middleware\SingleStoreMiddleware::class,
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
