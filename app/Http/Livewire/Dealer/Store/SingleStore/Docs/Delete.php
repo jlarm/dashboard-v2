fbee2b481c821c09cb2b3d987c898ce8a3eed77f
Joe Lohr <jlohr@autorisknow.com>
2023-10-03 14:01:21 -0500

    Added delete to multi store docs

---
+++ app/Http/Livewire/Dealer/Store/SingleStore/Docs/Delete.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Livewire\Dealer\Store\SingleStore\Docs;
+
+use App\Models\DealerDoc;
+use Filament\Notifications\Notification;
+use WireElements\Pro\Components\Modal\Modal;
+
+class Delete extends Modal
+{
+    public $doc;
+
+    public function mount(DealerDoc $doc)
+    {
+        $this->doc = $doc;
+    }
+
+    public function delete()
+    {
+        try {
+            \Storage::disk('dealer-docs')->delete($this->doc->file_path);
+
+            $this->doc->delete();
+
+            $this->emit('saved');
+
+            $this->close();
+
+            Notification::make()
+                ->title('Document Deleted Successfully!')
+                ->success()
+                ->send();
+        } catch(\Exception $e) {
+            \Log::error($e);
+            $this->addError('file', 'An error occurred while deleting the file.');
+        }
+    }
+    public function render()
+    {
+        return view('livewire.dealer.store.single-store.docs.delete');
+    }
+}
