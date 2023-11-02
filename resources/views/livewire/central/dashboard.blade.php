e77cb8429b8bf1e72a73bed4aeb5410bad1a2cc1
Joe Lohr <jlohr@autorisknow.com>
2023-11-02 09:36:33 -0500

    Central routes update

--- resources/views/livewire/central/dashboard.blade.php
+++ resources/views/livewire/central/dashboard.blade.php
@@ -1,6 +1,6 @@
 <div class="grid grid-cols-1 lg:grid-cols-8 gap-4">
     <div class="bg-white rounded-md p-6 col-span-4">
-        <h3 class="text-xl font-bold leading-none tracking-tight text-neutral-900">Upcoming Eventsz</h3>
+        <h3 class="text-xl font-bold leading-none tracking-tight text-neutral-900">Upcoming Events</h3>
         <livewire:central.event.index/>
         @can('delete-dealerships')
             <x-primary-button onclick="Livewire.emit('modal.open', 'central.event.create')">Add Event
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
 use App\Http\Controllers\Central\Course\CourseResultsController;
-use App\Http\Controllers\Central\Course\QuizController;
-use App\Http\Controllers\Central\Course\ShowController;
 use App\Http\Controllers\Central\Dealership\CreateController;
 use App\Http\Controllers\Central\EmployeeController;
-use App\Http\Controllers\Central\Role\EditController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Livewire\Central\Course\Index;
+use App\Http\Livewire\Central\Course\Quiz;
+use App\Http\Livewire\Central\Course\Show;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('welcome'); })->name('home');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
 
-    Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
+    Route::get('/dashboard', \App\Http\Livewire\Central\Dashboard::class)->name('dashboard');
 
     Route::get('/dealerships', function () { return view('central.dealership.index'); })->name('dealerships.index');
     Route::get('dealerships/create', function () { return view('central.dealership.create'); })->name('dealerships.create');
@@ -24,10 +24,14 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('courses', function () { return view('central.course.index'); })->name('courses.index');
-    Route::get('courses/{course:slug}', ShowController::class)->name('courses.show');
-    Route::get('courses/{course:slug}/quiz', QuizController::class)->name('courses.quiz');
+    Route::get('courses', Index::class)->name('courses.index');
+    Route::get('courses/{course:slug}', Show::class)->name('courses.show');
+    Route::get('courses/{course:slug}/quiz', Quiz::class)->name('courses.quiz');
     Route::post('courses/{course:slug}/quiz', CourseResultsController::class)->name('courses.quiz.store');
+
+    Route::get('/update', function() {
+        $course = \App\Models\Course::findOrFail(1)->update(['name' => 'NESHAP 6-H TEST']);
+    });
 
 });
 
@@ -35,14 +39,24 @@ Route::get('employees/create', [UserController::class, 'create'])->middleware('s
 Route::post('employees/store', [UserController::class, 'store'])->name('employees.store');
 
 Route::group(['middleware' => ['can:delete-users', 'auth', 'verified']], function () {
-    Route::get('employees', [EmployeeController::class, 'index'])->name('employees.index');
+    Route::get('employees', \App\Http\Livewire\Central\Employee\Index::class)->name('employees.index');
     Route::get('/employees/deleted', function () { return view('central.employee.deleted'); })->name('employees.deleted');
     Route::get('employees/invite', [EmployeeController::class, 'create'])->name('invite.create');
     Route::post('employees/invite', [EmployeeController::class, 'send'])->name('invite.send');
     Route::get('employees/{user}', [EmployeeController::class, 'show'])->name('employees.view');
-    Route::get('roles', function () { return view('central.role.index'); })->name('role.index');
-    Route::get('roles/create', function () { return view('central.role.create'); })->name('role.create');
-    Route::get('roles/{role:id}', EditController::class)->name('role.edit');
+
+    Route::get('roles', \App\Http\Livewire\Central\Role\Index::class)->name('role.index');
+    Route::get('roles/{role:id}', \App\Http\Livewire\Central\Role\Edit::class)->name('role.edit');
+
+    Route::get('permissions', \App\Http\Livewire\Central\Permission\Index::class)->name('permission.index');
+    Route::get('permissions/{permission:id}', \App\Http\Livewire\Central\Permission\Edit::class)->name('permission.edit');
+
+    Route::get('departments', \App\Http\Livewire\Central\Department\Index::class)->name('department.index');
+    Route::get('departments/{department:id}', \App\Http\Livewire\Central\Department\Edit::class)->name('department.edit');
+
+    Route::get('course-management', \App\Http\Livewire\Central\CourseManagement\Index::class)->name('course-management.index');
+    Route::get('course-management/{course:slug}', \App\Http\Livewire\Central\CourseManagement\Edit::class)->name('course-management.edit');
+    Route::get('course-management/quiz/{course:slug}', \App\Http\Livewire\Central\CourseManagement\EditQuiz::class)->name('course-management.edit-quiz');
 });
 
 require __DIR__.'/auth.php';
