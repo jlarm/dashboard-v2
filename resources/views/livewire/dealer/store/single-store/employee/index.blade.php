76e481f94de5177a997ad8cbe14a04e190a32200
Joe Lohr <jlohr@autorisknow.com>
2023-11-02 15:57:06 -0500

    Added export email to multi stores and allow consultants to test

--- resources/views/livewire/dealer/store/single-store/employee/index.blade.php
+++ resources/views/livewire/dealer/store/single-store/employee/index.blade.php
@@ -6,7 +6,7 @@
                 <div class="w-full max-w-xs flex items-center gap-x-3">
                     <label>
                         <input type="search" wire:model="search" placeholder="Search"
-                               class="flex w-full h-10 px-3 py-2 text-sm bg-white border rounded-md border-neutral-200 ring-offset-background placeholder:text-neutral-500 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50"/>
+                               class="flex w-56 h-10 px-3 py-2 text-sm bg-white border rounded-md border-neutral-200 ring-offset-background placeholder:text-neutral-500 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50"/>
                     </label>
                     <div>
                         <div
@@ -104,6 +104,29 @@
                             </div>
                         </div>
                     </div>
+                    @can('create-dealerships')
+                        @if($showIncompleteCourseUsers && $selectedDepartment)
+                            <div>
+                                <form wire:submit.prevent="generateCsv" class="flex space-x-3">
+                                    <label>
+                                        <input type="email" wire:model.defer="email"
+                                               placeholder="Enter Manager Email Address"
+                                               class="flex w-full w-56 h-10 px-3 py-2 text-sm bg-white border rounded-md border-neutral-200 ring-offset-background placeholder:text-neutral-500 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50"/>
+                                    </label>
+                                    <x-primary-button>
+                                        <svg wire:loading class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                                    stroke-width="4"></circle>
+                                            <path class="opacity-75" fill="currentColor"
+                                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                        Send
+                                    </x-primary-button>
+                                </form>
+                            </div>
+                        @endif
+                    @endcan
                 </div>
                 <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                     <x-primary-button onclick="Livewire.emit('modal.open', 'dealer.employee.invite')">Add Employee
