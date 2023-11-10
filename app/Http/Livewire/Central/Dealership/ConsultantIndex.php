e550be85b20d0a35d1e28a3f8afb0123599c53f0
Joe Lohr <jlohr@autorisknow.com>
2023-11-10 10:28:17 -0500

    Give access to dealer demo to consultants

--- app/Http/Livewire/Central/Dealership/ConsultantIndex.php
+++ app/Http/Livewire/Central/Dealership/ConsultantIndex.php
@@ -12,7 +12,10 @@ class ConsultantIndex extends Component
     public function render()
     {
         return view('livewire.central.dealership.consultant-index', [
-            'dealerships' => Dealership::where('user_id', auth()->user()->id)->get(),
+            'dealerships' => Dealership::query()
+                ->where('id', auth()->user()->id)
+                ->orWhere('id', 'e44653a5-c049-4be0-92e3-b8aacea4bf20')
+                ->get(),
         ]);
     }
 }
