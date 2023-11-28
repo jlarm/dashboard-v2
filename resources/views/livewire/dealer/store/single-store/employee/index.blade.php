221216841d4d889774fc081c0ef4a79c1ab0f809
Joe Lohr <jlohr@autorisknow.com>
2023-11-28 12:38:17 -0600

    open invites public

--- resources/views/livewire/dealer/store/single-store/employee/index.blade.php
+++ resources/views/livewire/dealer/store/single-store/employee/index.blade.php
@@ -131,14 +131,12 @@
                 <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                     <x-primary-button onclick="Livewire.emit('modal.open', 'dealer.employee.invite')">Add Employee
                     </x-primary-button>
-                    @if(auth()->user()->name === 'Joe Lohr')
-                        <a
-                            class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
-                            href="{{ route('dealer.stores.employees.open-invites', $store) }}"
-                        >
-                            Open Invites
-                        </a>
-                    @endif
+                    <a
+                        class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
+                        href="{{ route('dealer.stores.employees.open-invites', $store) }}"
+                    >
+                        Open Invites
+                    </a>
                 </div>
             </div>
             <div class="mt-8 flow-root">
