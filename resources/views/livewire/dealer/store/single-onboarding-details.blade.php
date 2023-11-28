f12dcd01f4675c83f6c32fc17122afb96c4c26b3
Joe Lohr <jlohr@autorisknow.com>
2023-11-28 10:45:51 -0600

    compliance email available to consultants

--- resources/views/livewire/dealer/store/single-onboarding-details.blade.php
+++ resources/views/livewire/dealer/store/single-onboarding-details.blade.php
@@ -15,10 +15,8 @@
                               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
                 </x-primary-button>
-                @if(auth()->user()->name === 'Joe Lohr')
-                    @if(!$store->user_submitted)
-                        <livewire:dealer.settings.send-compliance-email-link :store="$store"/>
-                    @endif
+                @if(!$store->user_submitted)
+                    <livewire:dealer.settings.send-compliance-email-link :store="$store"/>
                 @endif
             @endcan
         </div>
