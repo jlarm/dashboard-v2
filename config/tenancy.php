be035035444390df39c0cb844741823bbd11920f
Joe Lohr <jlohr@autorisknow.com>
2023-12-05 15:09:28 -0600

    updated tenancy config

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,7 +16,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'dashboard-v2.test',
+        'armp.app',
     ],
 
     /**
