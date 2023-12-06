b0d3947e69b6d4e12fd6a7a0894a037665e00b25
Joe Lohr <jlohr@autorisknow.com>
2023-12-06 07:32:12 -0600

    Temp hide cert button

--- resources/views/dealer/course/index.blade.php
+++ resources/views/dealer/course/index.blade.php
@@ -3,7 +3,7 @@
         <div class="min-w-0 flex-1">
             <h1 class="text-4xl font-bold text-arm-blue-900 sm:truncate leading-normal">{{ __('Courses') }}</h1>
         </div>
-        <livewire:dealer.course.dot-cert />
+{{--        <livewire:dealer.course.dot-cert />--}}
     </div>
     <div class="px-6">
         <div class="border rounded-md">
