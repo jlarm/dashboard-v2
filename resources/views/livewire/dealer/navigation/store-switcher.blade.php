8c0a8abfe11542a243a6751e0b3430ad056ebc40
jlarm <117611704+jlarm@users.noreply.github.com>
2023-12-20 07:57:37 -0600

    Update store-switcher.blade.php

--- resources/views/livewire/dealer/navigation/store-switcher.blade.php
+++ resources/views/livewire/dealer/navigation/store-switcher.blade.php
@@ -18,14 +18,14 @@
      x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
      x-id="['dropdown-button']"
     >
-        <input id="combobox" value="{{ $currentStore ? $currentStore : 'Select a Store'  }}" type="text" readonly class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-arm-blue-600 sm:text-sm sm:leading-6" role="combobox" aria-controls="options" aria-expanded="false">
+        <input id="combobox" value="{{ $currentStore ? $currentStore : 'Select a Store'  }}" type="text" readonly class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-arm-blue-600 sm:text-sm sm:leading-6" role="combobox" aria-controls="options" aria-expanded="false">
         <button
             x-ref="button"
             x-on:click="toggle()"
             :aria-expanded="open"
             :aria-controls="$id('dropdown-button')"
             type="button"
-            class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
+            class="absolute w-full inset-y-0 right-0 flex items-center justify-end rounded-r-md px-2 focus:outline-none"
         >
             <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" />
@@ -41,10 +41,10 @@
             style="display: none;"
             class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" id="options" role="listbox">
             @foreach($stores as $store)
-                <li class="relative cursor-default select-none py-2 pl-3 text-gray-900 hover:bg-gray-50" id="option-0" role="option" tabindex="-1">
+                <li class="relative cursor-default select-none p-2 text-gray-900 hover:bg-gray-50" id="option-0" role="option" tabindex="-1">
                     <a href="{{ route('dealer.stores.home', $store) }}">
                         <div class="flex items-center">
-                            <span class="ml-3 truncate">{{ $store->name }}</span>
+                            <span>{{ Str::limit($store->name, 25) }}</span>
                         </div>
                         @if($currentStore === $store->name)
                             <span class="absolute inset-y-0 right-0 flex items-center pr-4 text-arm-blue-600">
