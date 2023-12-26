bae5e8194a220a7a94f1e0d94e1b233acd3381ad
jlarm <117611704+jlarm@users.noreply.github.com>
2023-12-26 12:51:48 -0600

    temp hid docs

--- resources/views/livewire/central/docs/index.blade.php
+++ resources/views/livewire/central/docs/index.blade.php
@@ -16,40 +16,40 @@
     </div>
     <div class="grid grid-cols-1 @can('delete-dealerships') md:grid-cols-2 @endcan gap-5 mt-5">
         <div class="border rounded-md p-5 bg-white">
-            <ul role="list" class="divide-y divide-gray-100">
-                @forelse($docs as $doc)
-                    <li class="flex justify-between gap-x-6 py-5">
-                        <div class="flex min-w-0 gap-x-4">
-                            <div class="min-w-0 flex-auto">
-                                <p class="text-sm font-semibold leading-6 text-gray-900">{{ $doc->title }}</p>
-                            </div>
-                        </div>
-                        <div class="space-x-5 flex flex-col md:flex-row justify-end">
-                            <a target="_blank"
-                               href="https://central-docs.nyc3.cdn.digitaloceanspaces.com/{{ $doc->file_name }}"
-                               download
-                               class="text-sm leading-6 text-gray-900">
-                                Download
-                            </a>
-                            @can('delete-dealerships')
-                                <button
-                                    class="text-red-500 text-sm"
-                                    wire:click="$emit('modal.open', 'central.docs.delete',  @js(['doc' => $doc->id]))"
-                                >
-                                    Delete
-                                </button>
-                            @endcan
-                        </div>
-                    </li>
-                @empty
-                    <li class="flex justify-between gap-x-6 py-5">
-                        <p class="text-sm font-semibold leading-6 text-gray-900">No documents have been uploaded</p>
-                    </li>
-                @endforelse
-            </ul>
-            <div>
-                {{ $docs->links() }}
-            </div>
+{{--            <ul role="list" class="divide-y divide-gray-100">--}}
+{{--                @forelse($docs as $doc)--}}
+{{--                    <li class="flex justify-between gap-x-6 py-5">--}}
+{{--                        <div class="flex min-w-0 gap-x-4">--}}
+{{--                            <div class="min-w-0 flex-auto">--}}
+{{--                                <p class="text-sm font-semibold leading-6 text-gray-900">{{ $doc->title }}</p>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="space-x-5 flex flex-col md:flex-row justify-end">--}}
+{{--                            <a target="_blank"--}}
+{{--                               href="https://central-docs.nyc3.cdn.digitaloceanspaces.com/{{ $doc->file_name }}"--}}
+{{--                               download--}}
+{{--                               class="text-sm leading-6 text-gray-900">--}}
+{{--                                Download--}}
+{{--                            </a>--}}
+{{--                            @can('delete-dealerships')--}}
+{{--                                <button--}}
+{{--                                    class="text-red-500 text-sm"--}}
+{{--                                    wire:click="$emit('modal.open', 'central.docs.delete',  @js(['doc' => $doc->id]))"--}}
+{{--                                >--}}
+{{--                                    Delete--}}
+{{--                                </button>--}}
+{{--                            @endcan--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                @empty--}}
+{{--                    <li class="flex justify-between gap-x-6 py-5">--}}
+{{--                        <p class="text-sm font-semibold leading-6 text-gray-900">No documents have been uploaded</p>--}}
+{{--                    </li>--}}
+{{--                @endforelse--}}
+{{--            </ul>--}}
+{{--            <div>--}}
+{{--                {{ $docs->links() }}--}}
+{{--            </div>--}}
         </div>
         @can('delete-dealerships')
         <div class="border rounded-md p-5">
