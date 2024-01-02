97ba78fa8786587f116c77bb18817160789b153e
jlarm <117611704+jlarm@users.noreply.github.com>
2024-01-02 13:30:53 -0600

    Update tenancy.php

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,7 +16,8 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'armp.app',
+        'localhost',
+        env('APP_DOMAIN'),
     ],
 
     /**
