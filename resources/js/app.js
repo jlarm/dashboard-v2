72b27c6541507dce1b10b29eb7f7258af1e87920
jlarm <117611704+jlarm@users.noreply.github.com>
2024-01-02 14:47:08 -0600

    Remove Filament forms from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 import mask from '@alpinejs/mask';
 import persist from '@alpinejs/persist'
-import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
 import AlpineFloatingUI from '@awcodes/alpine-floating-ui'
 import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm'
 import collapse from '@alpinejs/collapse'
@@ -11,7 +10,6 @@ import collapse from '@alpinejs/collapse'
 Alpine.plugin(focus);
 Alpine.plugin(mask);
 Alpine.plugin(persist);
-Alpine.plugin(FormsAlpinePlugin)
 Alpine.plugin(AlpineFloatingUI)
 Alpine.plugin(NotificationsAlpinePlugin)
 Alpine.plugin(collapse)
