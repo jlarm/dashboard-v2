fd91f412312e288b39596601695ff1d97921117b
jlarm <117611704+jlarm@users.noreply.github.com>
2024-01-16 11:36:52 -0600

    Update index-item.blade.php

--- resources/views/livewire/dealer/employee/index-item.blade.php
+++ resources/views/livewire/dealer/employee/index-item.blade.php
@@ -8,7 +8,7 @@
     {{--            >--}}
     {{--        </td>--}}
     {{--    @endcan--}}
-    <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+    <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500">
         {{ Str::headline($user->name) }}
     </td>
     <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
