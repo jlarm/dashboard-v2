b7f68b0eafe03f0d7b5956469696ffabf4437abd
jlarm <117611704+jlarm@users.noreply.github.com>
2024-01-18 13:41:18 -0600

    Fixed horizontal scroll on vendors index

--- resources/views/livewire/dealer/store/single-store/vendor/index.blade.php
+++ resources/views/livewire/dealer/store/single-store/vendor/index.blade.php
@@ -12,7 +12,7 @@
         @endcan
     </div>
     <div class="bg-white border rounded-md p-6">
-        <div class="-mx-4 md:-mx-0 -my-2 md:-my-0 overflow-x-auto">
+        <div class="-mx-4 md:-mx-0 -my-2 md:-my-0">
             <div class="inline-block min-w-full align-middle">
                 <table class="min-w-full divide-y divide-gray-300">
                     <thead>
