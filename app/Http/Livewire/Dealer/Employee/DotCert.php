598d04059622043a6007e079b08a42c24e063ceb
jlarm <117611704+jlarm@users.noreply.github.com>
2024-03-27 10:37:16 -0500

    Added DOT Cert generate for consultant

---
+++ app/Http/Livewire/Dealer/Employee/DotCert.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Livewire\Dealer\Employee;
+
+use App\Models\Certificate;
+use App\Models\Dealer\Course;
+use App\Models\Dealer\CourseResults;
+use App\Models\User;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Spatie\Browsershot\Browsershot;
+
+class DotCert extends Component
+{
+    public User $user;
+    public $showCertButton;
+
+    public function mount()
+    {
+        if (! $this->passingGrades() || $this->user->certificates()->where('course_name', 'DOT Hazardous Materials Transportation')->exists()) {
+            $this->showCertButton = false;
+        } else {
+            if ($this->passingGrades()->passed && $this->passingGrades()?->created_at->diffInDays(now()) <= 365) {
+                $this->showCertButton = true;
+            } else {
+                $this->showCertButton = false;
+            }
+        }
+    }
+
+    private function passingGrades()
+    {
+        $course = Course::query()
+            ->where('slug', 'dot-hazardous-materials-transportation-shipping-papers-emergency-response-and-placarding')
+            ->latest()
+            ->pluck('id')
+            ->first();
+
+        return CourseResults::query()
+            ->where('user_id', $this->user->id)
+            ->where('course_id', $course)
+            ->first();
+    }
+
+    public function download(Request $request)
+    {
+        $html = view('dealer.course.CertDownloadView', [
+            'user' => $this->user,
+            'store' => $request->get('store')?->name ?? tenant('name'),
+            'passed_on' => $this->passingGrades()->created_at->format('F d, Y'),
+        ])->render();
+
+        $pdf = Browsershot::html($html)->landscape()->pdf();
+
+        $fileName = Str::slug($this->user->name).'-'.now()->format('m-d-Y').'-dot-certificate.pdf';
+
+        Storage::disk('local')->put($fileName, $pdf);
+
+        $localFile = Storage::disk('local')->get($fileName);
+
+        Storage::disk('armp-certs')->put(tenant('id').'/'.$this->user->id.'/'.$fileName, $localFile);
+
+        Storage::delete($fileName);
+
+        Certificate::create([
+            'user_id' => $this->user->id,
+            'course_name' => 'DOT Hazardous Materials Transportation',
+            'file_name' => $fileName,
+        ]);
+
+        $this->showCertButton = false;
+
+        Notification::make()
+            ->title('Certificate Generated Successfully!')
+            ->body('You can find your certificate in the Certificates section of your profile.')
+            ->icon('heroicon-o-document-text')
+            ->iconColor('success')
+            ->success()
+            ->actions([
+                Action::make('view-profile')
+                    ->button()
+                    ->color('primary')
+                    ->url(route('dealer.profile.edit')),
+            ])
+            ->send();
+
+    }
+
+    public function render()
+    {
+        return view('livewire.dealer.employee.dot-cert');
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //        Model::preventLazyLoading(! $this->app->isProduction());
+//        Model::preventLazyLoading(! $this->app->isProduction());
 
         view()->composer('layouts.top-bar', function ($view) {
             $view->with('current_locale', app()->getLocale());
