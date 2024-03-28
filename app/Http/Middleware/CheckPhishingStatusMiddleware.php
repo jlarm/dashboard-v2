bd7ca5e2aa08a6e9606c082e436b26d9efc37864
jlarm <117611704+jlarm@users.noreply.github.com>
2024-03-27 20:23:49 -0500

    temp disable phish middleware

--- app/Http/Middleware/CheckPhishingStatusMiddleware.php
+++ app/Http/Middleware/CheckPhishingStatusMiddleware.php
@@ -5,14 +5,12 @@ namespace App\Http\Middleware;
 use App\Models\Dealer\Store;
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckPhishingStatusMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        $enabled = Store::first()->phishing_is_enabled;
-
-        $request->session()->put('phishing_is_enabled', $enabled);
 
         return $next($request);
     }
