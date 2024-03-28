6b7a97d645d0d51f1a87e6c9e12edf5013605e1a
jlarm <117611704+jlarm@users.noreply.github.com>
2024-03-27 20:10:42 -0500

    Phishing (#78)
    * Phishing Index
    
    * All views for phishing sim, need to finish form
    
    * Show and create sims
    
    * Display results from db instead of api
    
    * Update to campaign timeline relationship
    
    * view updates
    
    * Apply Laravel coding style
    
    Shift automatically applies the Laravel coding style - which uses the PSR-12 coding style as a base with some minor additions.
    
    You may customize the code style applied by configuring [Pint](https://laravel.com/docs/pint), [PHP CS Fixer](https://github.com/FriendsOfPHP/PHP-CS-Fixer), or [PHP CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) for your project root.
    
    For more information on customizing the code style applied by Shift, [watch this short video](https://laravelshift.com/videos/shift-code-style).
    
    * Global Facades
    
    * Convert route array to fluent methods
    
    * Add PHP type hints
    
    * Shift cleanup
    
    * Removed middleware cache
    
    * Add PHP type hints
    
    * Shift cleanup
    
    ---------
    
    Co-authored-by: Shift <shift@laravelshift.com>

--- app/Http/Middleware/CheckPhishingStatusMiddleware.php
+++ app/Http/Middleware/CheckPhishingStatusMiddleware.php
@@ -5,16 +5,13 @@ namespace App\Http\Middleware;
 use App\Models\Dealer\Store;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckPhishingStatusMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $enabled = Cache::remember('phishing_is_enabled', now()->addMinutes(10), function () {
-            return Store::first()->phishing_is_enabled;
-        });
+        $enabled = Store::first()->phishing_is_enabled;
 
         $request->session()->put('phishing_is_enabled', $enabled);
 
