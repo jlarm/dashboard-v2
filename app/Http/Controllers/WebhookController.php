5b8d67daed77ca8ba5868f4d9cf7cab668d9763e
jlarm <117611704+jlarm@users.noreply.github.com>
2024-03-28 15:50:55 -0500

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,25 +12,23 @@ class WebhookController extends Controller
 {
     public function gophish(Request $request): JsonResponse
     {
-//        Timeline::create([
-//            'campaign_id' => $request->input('campaign_id'),
-//            'email' => $request->input('email'),
-//            'time' => $request->input('time'),
-//            'message' => $request->input('message'),
-//            'details' => $request->input('details'),
-//        ]);
-//
-//        $campaign = PhishingCampaign::where('campaign_id', $request->input('campaign_id'))->first();
-//
-//        match ($request->input('message')) {
-//            'Email Opened' => $campaign->increment('emails_opened'),
-//            'Clicked Link' => $campaign->increment('links_clicked'),
-//            'Submitted Data' => $campaign->increment('data_submitted'),
-//            'Email Reported' => $campaign->increment('emails_reported'),
-//        };
-//
-//        return response()->json(['status' => 'success']);
-        // return request
-        return response()->json($request->all());
+        Timeline::create([
+            'campaign_id' => $request->input('campaign_id'),
+            'email' => $request->input('email'),
+            'time' => $request->input('time'),
+            'message' => $request->input('message'),
+            'details' => $request->input('details'),
+        ]);
+
+        $campaign = PhishingCampaign::where('campaign_id', $request->input('campaign_id'))->first();
+
+        match ($request->input('message')) {
+            'Email Opened' => $campaign->increment('emails_opened'),
+            'Clicked Link' => $campaign->increment('links_clicked'),
+            'Submitted Data' => $campaign->increment('data_submitted'),
+            'Email Reported' => $campaign->increment('emails_reported'),
+        };
+
+        return response()->json(['status' => 'success']);
     }
 }
