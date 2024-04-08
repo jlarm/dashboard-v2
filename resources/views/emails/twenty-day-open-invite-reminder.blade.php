21ff6dd77b8ed42d1f26716586386a2dd1719fac
jlarm <117611704+jlarm@users.noreply.github.com>
2024-04-08 09:43:55 -0500

    Fixed formatting issue in twenty day invite reminder email

--- resources/views/emails/twenty-day-open-invite-reminder.blade.php
+++ resources/views/emails/twenty-day-open-invite-reminder.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
-    # Hi {{ $name }}
-    This is your final reminder to join the {{ $company }} compliance dashboard. Please click the link below to finish your registration.
-    If you do not complete your registration within the next 10 days, your invitation will be deleted.
-    @component('mail::button', ['url' => $link])
-        Complete Registration
-    @endcomponent
-    Thanks,
-    {{ $company }}
+# Hi {{ $name }}
+This is your final reminder to join the {{ $company }} compliance dashboard. Please click the link below to finish your registration.
+If you do not complete your registration within the next 10 days, your invitation will be removed.
+@component('mail::button', ['url' => $link])
+    Complete Registration
+@endcomponent
+Thanks,
+{{ $company }}
 @endcomponent
