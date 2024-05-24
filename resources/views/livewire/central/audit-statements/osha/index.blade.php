202bb05aa43fb51bce70b9970090fecc2f646118
jlarm <jlohr@autorisknow.com>
2024-05-24 09:09:55 -0500

    Consultant access to audit statements on central dashboard

--- resources/views/livewire/central/audit-statements/osha/index.blade.php
+++ resources/views/livewire/central/audit-statements/osha/index.blade.php
@@ -5,7 +5,9 @@
                 <h1 class="text-xl font-bold leading-none tracking-tight text-neutral-900">OSHA Violation Statements</h1>
             </div>
             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                @can('delete-users')
                 <a href="{{ route('osha-violations.create') }}" class="block rounded-md bg-arm-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-arm-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-arm-blue-600">Add Violation</a>
+                @endcan
             </div>
         </div>
         <div class="mt-8 flow-root">
