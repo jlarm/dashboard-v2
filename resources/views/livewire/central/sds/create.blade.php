fa331f5eb133fb96936570955f451b941efe3b02
jlarm <jlohr@autorisknow.com>
2024-06-11 13:52:18 -0500

    Update create.blade.php

--- resources/views/livewire/central/sds/create.blade.php
+++ resources/views/livewire/central/sds/create.blade.php
@@ -132,8 +132,17 @@
                 <div class="py-1 px-2 inline-flex items-center gap-x-2 text-xs font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
                     Select PDF
                 </div>
-                <input class="sr-only" id="file" type="file" wire:model="file" />
+                <input class="sr-only" id="file" type="file" wire:model.defer="file" />
             </label>
+            <div>
+                <svg wire:loading wire:target="file" class="animate-spin ml-1 h-5 w-5 text-arm-blue-500" xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </div>
             @if($file)
                 <p class="block text-xs text-gray-500 mt-2">{{ $file->getClientOriginalName() }}</p>
             @endif
