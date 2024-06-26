9341934c3b14b85e5b98a485bbada43d181dfa52
jlarm <jlohr@autorisknow.com>
2024-06-26 08:08:01 -0500

    Update SecurityHeadersMiddleware.php

--- app/Http/Middleware/SecurityHeadersMiddleware.php
+++ app/Http/Middleware/SecurityHeadersMiddleware.php
@@ -11,8 +11,6 @@ class SecurityHeadersMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Content-Security-Policy', "default-src 'self';");
-
         $response->headers->set('X-Frame-Options', 'DENY');
 
         $response->headers->set('X-XSS-Protection', '1; mode=block');
