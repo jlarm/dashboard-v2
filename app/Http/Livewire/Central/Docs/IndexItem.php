3df66e509d2c8204afd6e076dad2ecf11b73d5e4
jlarm <jlohr@autorisknow.com>
2024-06-26 11:12:51 -0500

    Central docs update

---
+++ app/Http/Livewire/Central/Docs/IndexItem.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire\Central\Docs;
+
+use App\Models\Document;
+use Livewire\Component;
+
+class IndexItem extends Component
+{
+    public Document $doc;
+
+    public function render()
+    {
+        return view('livewire.central.docs.index-item');
+    }
+}
