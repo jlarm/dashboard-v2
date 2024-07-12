08f42428b4fd4cf2ac412e1b7bcfd569a6971301
jlarm <117611704+jlarm@users.noreply.github.com>
2024-07-12 09:45:44 -0500

    Course show style updates

--- app/Http/Livewire/Dealer/Course/Show.php
+++ app/Http/Livewire/Dealer/Course/Show.php
@@ -3,14 +3,32 @@
 namespace App\Http\Livewire\Dealer\Course;
 
 use App\Models\Dealer\Course;
+use Illuminate\Support\Facades\URL;
 use Livewire\Component;
 
 class Show extends Component
 {
     public Course $course;
+    public $slides;
+
+    public function mount()
+    {
+        $this->slides = collect($this->course->slides);
+    }
+
+    public function quizLink()
+    {
+        return URL::temporarySignedRoute(
+            'courses.quiz',
+            now()->addMinutes(30),
+            ['course' => $this->course->slug]
+        );
+    }
 
     public function render()
     {
-        return view('livewire.dealer.course.show');
+        $quizLink = $this->quizLink();
+
+        return view('livewire.dealer.course.show', compact('quizLink'));
     }
 }
--- resources/views/livewire/central/course/show.blade.php
+++ resources/views/livewire/central/course/show.blade.php
@@ -45,16 +45,6 @@
                 <div class="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                     <div class="flex flex-col justify-center rounded-full overflow-hidden bg-arm-blue-600 text-xs text-white text-center whitespace-nowrap transition duration-500 dark:bg-blue-500" x-bind:style="'width: ' + percentage + '%;'"></div>
                 </div>
-{{--                <div class="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">--}}
-{{--                    <div class="flex flex-col justify-center rounded-full overflow-hidden bg-arm-blue-600 text-xs text-white text-center whitespace-nowrap dark:bg-blue-500 transition duration-500" x-bind:style="'width: ' + percentage + '%;'" x-text="percentage + '%'"></div>--}}
-{{--                </div>--}}
-                <!-- End Progress -->
-{{--                <div class="flex items-center justify-end gap-2">--}}
-{{--                    <span class="text-sm font-bold" x-text="percentage + '%'"></span>--}}
-{{--                    <div class="w-[200px] h-[15px] rounded bg-gray-100 overflow-hidden">--}}
-{{--                        <div x-bind:style="'width: ' + percentage + '%;'" class="h-full rounded-l bg-arm-orange-500"></div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
                 <button
                     x-show="activeSlide < slidesCount - 1"
                     @click="activeSlide++"
