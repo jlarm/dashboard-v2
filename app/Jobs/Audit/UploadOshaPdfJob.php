a6f05ec3bc7489d4ae1c62bba898aa00cde48786
jlarm <117611704+jlarm@users.noreply.github.com>
2024-07-16 12:49:05 -0500

    Update UploadOshaPdfJob.php

--- app/Jobs/Audit/UploadOshaPdfJob.php
+++ app/Jobs/Audit/UploadOshaPdfJob.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
-use Storage;
+use Illuminate\Support\Facades\Storage;
 
 class UploadOshaPdfJob implements ShouldQueue
 {
