bfead3a8df710f13f581ee74171d467cdb7072b4
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-17 15:16:05 -0500

    Only allow one role to be selected when editing employee

--- app/Http/Livewire/Dealer/Employee/Edit.php
+++ app/Http/Livewire/Dealer/Employee/Edit.php
@@ -12,95 +12,112 @@ use WireElements\Pro\Components\SlideOver\SlideOver;
 class Edit extends SlideOver
 {
     public $user;
-
     public $name;
-
     public $assignedStores;
-
     public $department;
-
     public $assignedRoles;
-
     public $qi;
-
     public $qiCount;
 
     public function mount(User $user)
+    {
+        $this->initializeUserData($user);
+    }
+
+    private function initializeUserData(User $user)
     {
         $this->user = $user;
         $this->name = $user->name;
         $this->assignedStores = $user->stores()->pluck('name')->toArray();
         $this->department = $user->department_id;
-        $this->assignedRoles = $user->getRoleNames()->toArray();
+        $this->assignedRoles = $user->roles()->whereNotIn('name', ['Qualified Individual'])->pluck('name')->toArray();
         $this->qi = $this->user->hasRole('Qualified Individual');
         $this->qiCount = Role::find(5)->users()->count();
     }
 
     public function updateUser()
     {
-        // Ensure at least one role is assigned
-        if (empty($this->assignedRoles)) {
-            Notification::make()
-                ->title('At least one role must be assigned.')
-                ->warning()
-                ->send();
-
+        if (count($this->assignedRoles) > 1) {
+            $this->sendNotification('Only one role can be assigned unless it includes "Qualified Individual".', 'warning');
             return;
         }
 
-        // Ensure at least one store is selected
-        if (empty($this->assignedStores)) {
-            Notification::make()
-                ->title('At least one store must be selected.')
-                ->warning()
-                ->send();
+        $this->updateUserData();
+        $this->syncUserRoles();
+        $this->assignQiRole();
+        $this->clearPermissionCache();
+        $this->emitRefreshEvents();
+        $this->closeWithSuccessNotification();
+    }
 
-            return;
-        }
-
+    private function updateUserData()
+    {
         $this->user->update([
             'department_id' => $this->department,
         ]);
 
         $this->user->stores()->sync(Store::whereIn('name', $this->assignedStores)->pluck('id')->toArray());
+    }
 
+    private function assignQiRole()
+    {
         if ($this->qi) {
-            if (! in_array('Qualified Individual', $this->assignedRoles)) {
-                $this->assignedRoles[] = 'Qualified Individual';
-            }
+            $this->user->assignRole('Qualified Individual');
         } else {
-            $key = array_search('Qualified Individual', $this->assignedRoles);
-            if ($key !== false) {
-                unset($this->assignedRoles[$key]);
-            }
+            $this->user->removeRole('Qualified Individual');
         }
+    }
 
+    private function syncUserRoles()
+    {
         $this->user->syncRoles($this->assignedRoles);
+    }
 
+    private function clearPermissionCache()
+    {
         app()->make(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+    }
 
+    private function emitRefreshEvents()
+    {
         $this->emitTo('dealer.employee.details', 'refreshEmployeeDetails');
         $this->emitTo('dealer.employee.course-results', 'refreshEmployeeDetails');
+    }
 
+    private function closeWithSuccessNotification()
+    {
         $this->close();
+        $this->sendNotification($this->user->name . ' successfully updated', 'success');
+    }
 
+    private function sendNotification($message, $type)
+    {
         Notification::make()
-            ->title($this->user->name.' successfully updated')
-            ->success()
+            ->title($message)
+            ->{$type}()
             ->send();
     }
 
     public function render()
     {
-        $rolesQuery = Role::query()
-            ->whereNotIn('name', ['super-admin', 'Admin', 'Consultant', 'Qualified Individual'])
-            ->orderBy('name');
-
         return view('livewire.dealer.employee.edit', [
             'stores' => Store::all(),
             'departments' => Department::all(),
-            'allRoles' => $rolesQuery->get(),
-            'qiAvailable' => User::role('Qualified Individual')->count() < 3 || $this->qi,
+            'allRoles' => $this->getAvailableRoles(),
+            'qiAvailable' => $this->isQiAvailable(),
         ]);
+    }
+
+    private function getAvailableRoles()
+    {
+        return Role::query()
+            ->whereNotIn('name', ['super-admin', 'Admin', 'Consultant', 'Qualified Individual'])
+            ->orderBy('name')
+            ->get();
+    }
+
+    private function isQiAvailable()
+    {
+        return User::role('Qualified Individual')->count() < 3 || $this->qi;
     }
 }
