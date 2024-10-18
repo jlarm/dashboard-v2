561f05242db610fc418a3c8bf70bfb91f7775198
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-18 14:27:58 -0500

    Update dashboard.blade.php

--- resources/views/dealer/dashboard.blade.php
+++ resources/views/dealer/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-dealer-app>
         <div class="p-5 space-y-5">
         @if(!tenant('locations'))
-            @can('create-users')
+            @can('create-stores')
 {{--             Audit Stats--}}
             <div class="grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-3 xl:gap-5">
                 <livewire:dealer.home.osha-stats/>
@@ -128,20 +128,20 @@
                     <livewire:dealer.home.group-rating />
             @endcan
         @endif
-        @cannot('create-users')
-                <div class="px-6 py-4 sm:flex sm:items-center sm:justify-between">
-                    <div class="min-w-0 flex-1">
-                        <h1 class="text-4xl font-bold text-arm-blue-900 sm:truncate leading-normal">{{ __('Courses') }}</h1>
-                    </div>
-                    <livewire:dealer.course.dot-cert />
+        @cannot('create-stores')
+            <div class="px-6 py-4 sm:flex sm:items-center sm:justify-between">
+                <div class="min-w-0 flex-1">
+                    <h1 class="text-4xl font-bold text-arm-blue-900 sm:truncate leading-normal">{{ __('Courses') }}</h1>
                 </div>
-                <div class="px-6">
-                    <div class="border rounded-md">
-                        <div class="p-6">
-                            <livewire:dealer.course.index/>
-                        </div>
+                <livewire:dealer.course.dot-cert />
+            </div>
+            <div class="px-6">
+                <div class="border rounded-md">
+                    <div class="p-6">
+                        <livewire:dealer.course.index/>
                     </div>
                 </div>
+            </div>
         @endcannot
     </div>
 </x-dealer-app>
