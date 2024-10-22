e2c3f04ea5698fc0bc1c6a2b95cdab88c1b04475
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-22 16:41:04 -0500

    Update view permissions

--- resources/views/livewire/dealer/audit/finance/single.blade.php
+++ resources/views/livewire/dealer/audit/finance/single.blade.php
@@ -113,6 +113,6 @@
                 <!-- End Empty State -->
             @endforelse
         </div>
-        <a class="inline-flex items-center px-4 py-2 bg-arm-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-arm-blue-700 focus:bg-arm-blue-700 active:bg-arm-blue-900 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2 transition ease-in-out duration-150 mt-5" href="{{ tenant('locations') ? route('dealer.stores.audits.finance.index', $store) : route('dealer.audit.finance.index') }}">Back</a>
+        <a class="inline-flex items-center px-4 py-2 bg-arm-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-arm-blue-700 focus:bg-arm-blue-700 active:bg-arm-blue-900 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2 transition ease-in-out duration-150 mt-5" href="{{ url()->previous() }}">Back</a>
     </div>
 </div>
