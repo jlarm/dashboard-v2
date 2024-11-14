df6531af64149615f2680d4690ada5e943edf41f
jlarm <117611704+jlarm@users.noreply.github.com>
2024-11-14 10:40:59 -0600

    Update notification.blade.php

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -9,7 +9,7 @@
     x-show="show && type && title && message"
     x-effect="if (show) setTimeout(function() {show = false}, 3000)"
     aria-live="assertive"
-    class="fixed inset-0 flex items-end px-4 py-6 sm:items-start z-50 sm:p-6"
+    class="fixed inset-0 flex items-end px-4 py-6 sm:items-start z-50 sm:p-6 pointer-events-none"
     x-transition:enter="transition ease-out duration-300"
     x-transition:enter-start="opacity-0 transform"
     x-transition:enter-end="opacity-100 transform"
@@ -56,7 +56,7 @@
                         <p class="mt-1 text-sm text-gray-500" x-text="message"></p>
                     </div>
                     <div class="ml-4 flex flex-shrink-0">
-                        <button type="button" @click="show = ! show" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2">
+                        <button type="button" @click="show = false" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-arm-blue-500 focus:ring-offset-2">
                             <span class="sr-only">Close</span>
                             <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                 <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
