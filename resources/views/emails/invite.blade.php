f7fa718ab0c66b9455d671102df95a56d2da67a9
jlarm <117611704+jlarm@users.noreply.github.com>
2024-12-23 09:18:46 -0600

    Update invite.blade.php

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,6 +1,8 @@
 @component('mail::message')
 # Hi {{ $name }}
 You have been invited to join the {{ $company }} compliance dashboard. Please click the link below to finish your registration.
+
+Please do not disregard this request. If you have any questions or concerns, please speak with your manager.
 @component('mail::button', ['url' => $link])
     Complete Registration
 @endcomponent
