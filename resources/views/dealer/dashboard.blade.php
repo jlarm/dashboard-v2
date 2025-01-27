0698c8f473860aa592b1b4202c1fc87edd51601d
jlarm <117611704+jlarm@users.noreply.github.com>
2025-01-27 10:48:07 -0600

    Update dashboard.blade.php

--- resources/views/dealer/dashboard.blade.php
+++ resources/views/dealer/dashboard.blade.php
@@ -130,7 +130,7 @@
         @endif
         @cannot('create-stores')
             <div class="p-5 bg-white border border-gray-200 shadow-sm rounded-xl">
-                <livewire:tenant.course.index />
+                <livewire:dealer.course.index/>
             </div>
         @endcannot
     </div>
