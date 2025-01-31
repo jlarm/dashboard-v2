2dce20a76cf09b1e15ea05433925ca23a4788e5f
Shift <shift@laravelshift.com>
2025-01-31 17:37:20 +0000

    Add PHP type hints

--- app/Http/Controllers/Central/DealerDocs/CreateController.php
+++ app/Http/Controllers/Central/DealerDocs/CreateController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Central\DealerDocs;
 
+use Illuminate\View\View;
 use App\Http\Controllers\Controller;
 use App\Models\DealerDoc;
 
 class CreateController extends Controller
 {
-    public function __invoke()
+    public function __invoke(): View
     {
         $this->authorize('create', DealerDoc::class);
 
