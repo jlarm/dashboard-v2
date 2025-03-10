546798432439fde3f836b52f17cf8cbaf53fa179
jlarm <117611704+jlarm@users.noreply.github.com>
2025-03-10 11:13:25 -0500

    Update index.blade.php

--- resources/views/livewire/central/dealership/index.blade.php
+++ resources/views/livewire/central/dealership/index.blade.php
@@ -9,7 +9,6 @@
             <div>
                 <div class="w-[200px]">
                     <select wire:model="perPage" id="location" name="location" class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-arm-blue-600 sm:text-sm/6">
-                        <option value="1">1 per page</option>
                         <option value="10">10 per page</option>
                         <option value="25">25 per page</option>
                         <option value="50">50 per page</option>
