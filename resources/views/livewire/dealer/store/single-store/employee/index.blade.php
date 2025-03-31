0f999f4af8b60ba7b196e145af4c600df93b0fd4
jlarm <117611704+jlarm@users.noreply.github.com>
2025-03-31 12:52:59 -0500

    Fix bug with not showing department filter for employee list

--- resources/views/livewire/dealer/store/single-store/employee/index.blade.php
+++ resources/views/livewire/dealer/store/single-store/employee/index.blade.php
@@ -91,10 +91,10 @@
                                 </label>
                             </div>
                         </div>
-                        @if(!auth()->user()->hasRole('Manager'))
+                        @if(auth()->user()->can('create-stores'))
                             <div>
                                 <label for="department"
-                                       class="text-sm font-medium leading-6 text-gray-950 dark:text-white">Department</label>
+                                       class="text-sm font-medium leading-6 text-gray-950">Department</label>
                                 <select wire:model="selectedDepartment" id="department" name="department"
                                         class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-arm-blue-600 sm:text-sm sm:leading-6">
                                     <option value="null">All</option>
