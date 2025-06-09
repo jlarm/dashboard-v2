591fab81c7fd78de8867f04dc6680dfadc431626
jlarm <117611704+jlarm@users.noreply.github.com>
2025-06-09 08:22:26 -0500

    chore: removed icons for audit status

--- resources/views/livewire/dealer/audit/finance/index-item.blade.php
+++ resources/views/livewire/dealer/audit/finance/index-item.blade.php
@@ -219,11 +219,6 @@
                         'bg-emerald-100 text-emerald-700' => $glbaViolationAudit->completed_date,
                         'bg-sky-100 text-sky-700' => !$glbaViolationAudit->completed_date
                     ])>
-                        @if($glbaViolationAudit->completed_date)
-                            <svg class="h-4 w-4 text-emerald-500" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                        @else
-                            <svg class="h-3 w-3 text-sky-500 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"> <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle> <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
-                        @endif
                         {{ $glbaViolationAudit->completed_date ? 'Complete' : 'In Progress' }}
                     </span>
                 </dd>
