3a4a6a9c643843c4ff66b4789773205c8f9c6f78
jlarm <117611704+jlarm@users.noreply.github.com>
2025-06-09 16:03:43 -0500

    chore: fixed search for glba audits

--- resources/views/livewire/dealer/audit/finance/edit.blade.php
+++ resources/views/livewire/dealer/audit/finance/edit.blade.php
@@ -213,7 +213,7 @@
         </form>
     </div>
     <div class="fixed bottom-5 right-5">
-        <button class="inline-flex h-14 w-14 items-center justify-center rounded-full bg-arm-blue-500" onclick="Livewire.emit('modal.open', 'dealer.audit.body-shop.modal')">
+        <button class="inline-flex h-14 w-14 items-center justify-center rounded-full bg-arm-blue-500" onclick="Livewire.emit('modal.open', 'dealer.audit.finance.modal')">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
