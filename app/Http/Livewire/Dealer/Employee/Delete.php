a8fe7f82c2384033edcf4b9ddd6cd444cf3caa19
jlarm <117611704+jlarm@users.noreply.github.com>
2025-06-17 10:46:02 -0500

    chore:update redirect after employee has been deleted

--- app/Http/Livewire/Dealer/Employee/Delete.php
+++ app/Http/Livewire/Dealer/Employee/Delete.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Livewire\Dealer\Employee;
 
+use App\Models\Dealer\Store;
 use App\Models\User;
 use Filament\Notifications\Notification;
 use WireElements\Pro\Components\Modal\Modal;
 
 class Delete extends Modal
 {
+    public Store $store;
     public $user;
 
     public function mount(User $user)
     {
+        $this->store = Store::find(app('currentStore'));
         $this->user = $user;
     }
 
@@ -25,7 +28,7 @@ class Delete extends Modal
                 ->success()
                 ->send();
 
-            return redirect()->route('dealer.employees.index');
+            return tenant('locations') ? redirect()->route('dealer.stores.employees', $this->store) : redirect()->route('dealer.employees.index');
         } catch (\Exception $e) {
             \Log::error($e);
             $this->addError('file', 'An error occurred while deleting the user.');
