2a9dfe71c4a2049114c9ee1daa40eca3ef93a601
jlarm <117611704+jlarm@users.noreply.github.com>
2025-08-13 10:52:09 -0500

    Logs Refactor

--- app/Http/Livewire/Dealer/Log/Index.php
+++ app/Http/Livewire/Dealer/Log/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Dealer\Log;
 
+use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Spatie\Activitylog\Models\Activity;
@@ -10,10 +11,24 @@ class Index extends Component
 {
     use WithPagination;
 
-    public function render()
+    public $selectedLog = null;
+
+    public function viewLogDetails($logId): void
+    {
+        $this->selectedLog = Activity::with('causer', 'subject')->find($logId);
+        $this->dispatchBrowserEvent('open-log-modal');
+    }
+
+    public function closeModal(): void
+    {
+        $this->selectedLog = null;
+        $this->dispatchBrowserEvent('close-log-modal');
+    }
+
+    public function render(): View
     {
         return view('livewire.dealer.log.index', [
-            'logs' => Activity::latest()->paginate(25),
+            'logs' => Activity::with('causer')->latest()->paginate(25),
         ])->layout('components.dealer-app');
     }
 }
