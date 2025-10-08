92251309bbe4d989f7f536780ee45b46c6b15b94
jlarm <117611704+jlarm@users.noreply.github.com>
2025-10-08 08:20:32 -0500

    Cyrisma migration and model

--- app/Models/Dealer/Cyrisma.php
+++ app/Models/Dealer/Cyrisma.php
@@ -11,14 +11,7 @@ class Cyrisma extends Model
 {
     protected $fillable = [
         'store_id',
-        'short_name',
         'instance_id',
-        'instance_url',
-    ];
-
-    protected $casts = [
-        'instance_id' => 'encrypted',
-        'instance_url' => 'encrypted',
     ];
 
     public function store(): BelongsTo
--- database/migrations/tenant/2025_10_08_131539_create_cyrismas_table.php
+++ database/migrations/tenant/2025_10_08_131539_create_cyrismas_table.php
@@ -12,10 +12,8 @@ return new class extends Migration {
     {
         Schema::create('cyrismas', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Store::class)->constrained()->cascadeOnDelete();
-            $table->string('short_name');
-            $table->text('instance_id');
-            $table->text('instance_url')->nullable();
+            $table->foreignIdFor(Store::class);
+            $table->string('instance_id');
             $table->timestamps();
         });
     }
