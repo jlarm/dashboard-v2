f01a7c9e3bdfefb210271e0cd453583e2de90d92
jlarm <117611704+jlarm@users.noreply.github.com>
2025-10-10 09:33:04 -0500

    User factory update

--- app/Http/Livewire/Central/Dashboard.php
+++ app/Http/Livewire/Central/Dashboard.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Livewire\Central;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.central.dashboard');
     }
