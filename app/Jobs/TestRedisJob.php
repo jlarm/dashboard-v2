1a7d66f1765a29070ee5e1653f3e636b421a14e6
jlarm <117611704+jlarm@users.noreply.github.com>
2025-10-28 13:31:13 -0500

    Create TestRedisJob.php

---
+++ app/Jobs/TestRedisJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class TestRedisJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // This is the core action: writing a distinct message to the log.
+        Log::info('*** REDIS QUEUE TEST SUCCESSFUL: TestRedisJob was processed! ***');
+    }
+}
