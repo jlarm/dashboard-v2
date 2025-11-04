4fe603fb28f32cad5eb2f5d4d5eaa42e7be4935b
jlarm <117611704+jlarm@users.noreply.github.com>
2025-11-04 13:30:05 -0600

    Updated expired date for course taken

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -21,6 +23,7 @@ class Course extends Model
         'slides',
         'questions',
         'video_id',
+        'years_expires',
     ];
     protected $casts = [
         'slides' => 'array',
