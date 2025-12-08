65f1c5a8291da2ab62b8fd7ac298635886338463
jlarm <117611704+jlarm@users.noreply.github.com>
2025-12-08 17:12:50 -0600

    query bug

--- app/Models/CourseUser.php
+++ app/Models/CourseUser.php
@@ -4,12 +4,40 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class CourseUser extends Pivot
 {
     public $incrementing = false;
     protected $table = 'course_user';
-    protected $primaryKey = null;
+    protected $primaryKey = ['user_id', 'course_id'];
     protected $fillable = ['user_id', 'course_id', 'type', 'assigned_by'];
+
+    protected function setKeysForSaveQuery($query): Builder
+    {
+        $keys = $this->getKeyName();
+        if (! is_array($keys)) {
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach ($keys as $keyName) {
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    protected function getKeyForSaveQuery(?string $keyName = null): mixed
+    {
+        if (is_null($keyName)) {
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
 }
