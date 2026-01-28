24127ea2986c586035ea5b4d8cd0423ccd34a96a
jlarm <117611704+jlarm@users.noreply.github.com>
2026-01-28 15:58:04 -0600

    Dealership list on central dashboard page

--- resources/views/livewire/central/dashboard.blade.php
+++ resources/views/livewire/central/dashboard.blade.php
@@ -1,11 +1,19 @@
-<div class="grid grid-cols-1 lg:grid-cols-8 gap-4">
-    <div class="bg-white rounded-md p-6 col-span-4">
-        <h3 class="text-xl font-bold leading-none tracking-tight text-neutral-900">Upcoming Events</h3>
-        <livewire:central.event.index/>
-        @role('super-admin')
-            <x-primary-button onclick="Livewire.emit('modal.open', 'central.event.create')">Add Event</x-primary-button>
-        @endrole
-    </div>
-    <div class="bg-white rounded-md p-4 col-span-4">
+<div>
+    <div class="max-w-md mx-auto">
+        <h1 class="text-lg text-arm-blue-900 font-bold border-b">Quick Links</h1>
+        <ul class="divide-y divide-gray-200">
+            @foreach($dealerships as $dealership)
+                <li class="flex items-center justify-between gap-x-6 py-3">
+                    <p class="text-sm">{{ $dealership->name }}</p>
+                    <a target="_blank" href="https://{{ $dealership->domain() }}/dashboard" class="relative items-center justify-center inline-flex gap-1 text-xs h-8 border border-zinc-200 rounded-md px-2 hover:bg-zinc-100 transition">
+                        View
+                        <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M17.5 6.5L6 18" />
+                            <path d="M8 6H18V16" />
+                        </svg>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
     </div>
 </div>
