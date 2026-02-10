4e54091085329a71eee6f05afd2f90a4ac79e180
jlarm <117611704+jlarm@users.noreply.github.com>
2026-02-10 11:00:31 -0600

    fix: N+1 for course listing

--- resources/views/livewire/dealer/course/index.blade.php
+++ resources/views/livewire/dealer/course/index.blade.php
@@ -5,7 +5,12 @@
     <div class="space-y-4">
         <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4">
             @forelse($courses as $course)
-                <livewire:dealer.course.index-item :course="$course" :key="$course->id"/>
+                <livewire:dealer.course.index-item
+                    :course="$course"
+                    :module1="$module1"
+                    :module2="$module2"
+                    :module3="$module3"
+                    :key="$course->id"/>
             @empty
                 <p>No Courses Available</p>
             @endforelse
