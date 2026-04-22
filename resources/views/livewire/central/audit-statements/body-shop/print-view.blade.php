18442efabef5f91c01a7b8926057ced85d03d7a5
jlarm <117611704+jlarm@users.noreply.github.com>
2026-04-22 11:39:30 -0500

    removed central livewire components

--- resources/views/livewire/central/audit-statements/body-shop/print-view.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="max-w-7xl mx-auto">
-    <div class="space-y-3">
-        @foreach($violations as $violation)
-            <p>{{ $loop->index + 1 }}. {{ $violation->statement }}</p>
-        @endforeach
-    </div>
-</div>
-
-<script>window.print()</script>
--- resources/views/livewire/central/audit-statements/osha/delete.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <x-danger-button wire:click.prevent="delete">Delete</x-danger-button>
-</div>
